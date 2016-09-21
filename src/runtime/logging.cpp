f2f0b2722e2cf53d982c5ebb52425e84278140e1
Heiko Becker <heirecka@exherbo.org>
2016-09-21 19:37:47 +0200

    Drop obsolete version check
    Frameworks already require Qt 5.5.0.
    
    REVIEW: 128994

--- src/runtime/logging.cpp
+++ src/runtime/logging.cpp
@@ -19,9 +19,5 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 #include "logging_p.h"
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 4, 0)
 // logging category for this framework, default: log stuff >= warning
 Q_LOGGING_CATEGORY(KGLOBALACCELD, "kglobalaccel-runtime", QtWarningMsg)
-#else
-Q_LOGGING_CATEGORY(KGLOBALACCELD, "kglobalaccel-runtime")
-#endif
