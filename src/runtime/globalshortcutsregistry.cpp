Sonic-DE/sonic-frameworks-keybind

Let GlobalShortcutsRegistry manage all components
Component and KServiceActionComponent are always created with the 3rd
arg being the static instance returned by GlobalShortcutsRegistry::self(),
i.e. even though the constructors take a nullptr for the 3rd arg, this
doesn't actually happen anywhere in this code. Also I couldn't find any
code outside of KGlobalAccel that instantiates either of those classes.

Make GlobalShortcutsRegistry create those objects. This makes it clearer it
owns/manages them.

Rename addComponent() to registerComponent().

Remove GlobalShortcutsRegistry::allMainComponents(), not used any more.

src/runtime/globalshortcutsregistry.cpp
@@ -110,17 +110,10 @@ GlobalShortcutsRegistry::~GlobalShortcutsRegistry()
     _keys_count.clear();
 }
 
-Component *GlobalShortcutsRegistry::addComponent(Component *component)
+Component *GlobalShortcutsRegistry::registerComponent(Component *component)
 {
-    auto it = findByName(component->uniqueName());
-    if (it != m_components.cend()) {
-        Q_ASSERT_X(false, "GlobalShortcutsRegistry::addComponent", "component already registered");
-        return *it;
-    }
-
     m_components.push_back(component);
     QDBusConnection conn(QDBusConnection::sessionBus());
-
     conn.registerObject(component->dbusPath().path(), component, QDBusConnection::ExportScriptableContents);
     return component;
 }
@@ -130,11 +123,6 @@ void GlobalShortcutsRegistry::activateShortcuts()
     for (Component *component : m_components) {
         component->activateShortcuts();
     }
-}
-
-const std::vector<Component *> &GlobalShortcutsRegistry::allMainComponents() const
-{
-    return m_components;
 }
 
 QList<QDBusObjectPath> GlobalShortcutsRegistry::componentsDbusPaths() const
@@ -328,6 +316,34 @@ bool GlobalShortcutsRegistry::keyReleased(int keyQt)
     return false;
 }
 
+Component *GlobalShortcutsRegistry::createComponent(const QString &uniqueName, const QString &friendlyName)
+{
+    auto it = findByName(uniqueName);
+    if (it != m_components.cend()) {
+        Q_ASSERT_X(false, //
+                   "GlobalShortcutsRegistry::createComponent",
+                   QLatin1String("A Component with the name: %1, already exists").arg(uniqueName).toUtf8().constData());
+        return *it;
+    }
+
+    auto *c = registerComponent(new Component(uniqueName, friendlyName));
+    return c;
+}
+
+KServiceActionComponent *GlobalShortcutsRegistry::createSAComponent(const QString &uniqueName, const QString &friendlyName)
+{
+    auto it = findByName(uniqueName);
+    if (it != m_components.cend()) {
+        Q_ASSERT_X(false, //
+                   "GlobalShortcutsRegistry::createServiceActionComponent",
+                   QLatin1String("A Component with the name: %1, already exists").arg(uniqueName).toUtf8().constData());
+        return static_cast<KServiceActionComponent *>(*it);
+    }
+
+    auto *c = registerComponent(new KServiceActionComponent(uniqueName, friendlyName));
+    return static_cast<KServiceActionComponent *>(c);
+}
+
 void GlobalShortcutsRegistry::loadSettings()
 {
     const auto groupList = _config.groupList();
@@ -346,13 +362,10 @@ void GlobalShortcutsRegistry::loadSettings()
         // that
         const QString friendlyName = configGroup.readEntry("_k_friendly_name");
 
+        const bool isDesktop = groupName.endsWith(QLatin1String(".desktop"));
         // Create the component
-        Component *component = nullptr;
-        if (groupName.endsWith(QLatin1String(".desktop"))) {
-            component = new KServiceActionComponent(groupName, friendlyName, this);
-        } else {
-            component = new Component(groupName, friendlyName, this);
-        }
+        Component *component = isDesktop ? createSAComponent(groupName, friendlyName) //
+                                         : createComponent(groupName, friendlyName);
 
         // Now load the contexts
         const auto groupList = configGroup.groupList();
@@ -387,14 +400,14 @@ void GlobalShortcutsRegistry::loadSettings()
             continue;
         }
 
-        KDesktopFile f(file);
-        if (f.noDisplay()) {
+        KDesktopFile deskF(file);
+        if (deskF.noDisplay()) {
             continue;
         }
 
-        KServiceActionComponent *component = new KServiceActionComponent(fileName, f.readName(), this);
-        component->activateGlobalShortcutContext(QStringLiteral("default"));
-        component->loadFromService();
+        auto *actionComp = createSAComponent(fileName, deskF.readName());
+        actionComp->activateGlobalShortcutContext(QStringLiteral("default"));
+        actionComp->loadFromService();
     }
 }
 

src/runtime/globalshortcutsregistry.h
@@ -10,6 +10,7 @@
 #include "kglobalaccel.h"
 
 #include "component.h"
+#include "kserviceactioncomponent.h"
 
 #include <KSharedConfig>
 
@@ -56,11 +57,6 @@ public:
      * Activate all shortcuts having their application present.
      */
     void activateShortcuts();
-
-    /**
-     * Return a list of all main components
-     */
-    const std::vector<Component *> &allMainComponents() const;
 
     /**
      * Returns a list of D-Bus paths of registered Components.
@@ -140,11 +136,15 @@ public Q_SLOTS:
     void ungrabKeys();
 
 private:
+    friend class KGlobalAccelDPrivate;
     friend class Component;
     friend class KGlobalAccelInterface;
     friend class KGlobalAccelInterfaceV2;
 
-    Component *addComponent(Component *component);
+    Component *createComponent(const QString &uniqueName, const QString &friendlyName);
+    KServiceActionComponent *createSAComponent(const QString &uniqueName, const QString &friendlyName);
+
+    Component *registerComponent(Component *component);
     Component *takeComponent(Component *component);
 
     // called by the implementation to inform us about key presses

src/runtime/kglobalacceld.cpp
@@ -104,19 +104,26 @@ GlobalShortcut *KGlobalAccelDPrivate::findAction(const QString &_componentUnique
 
 Component *KGlobalAccelDPrivate::component(const QStringList &actionId) const
 {
-    // Get the component for the action. If we have none create a new one
-    Component *component = m_registry->getComponent(actionId.at(KGlobalAccel::ComponentUnique));
-    if (!component) {
-        if (actionId.at(KGlobalAccel::ComponentUnique).endsWith(QLatin1String(".desktop"))) {
-            component = new KServiceActionComponent(actionId.at(KGlobalAccel::ComponentUnique), actionId.at(KGlobalAccel::ComponentFriendly), m_registry);
-            component->activateGlobalShortcutContext(QStringLiteral("default"));
-            static_cast<KServiceActionComponent *>(component)->loadFromService();
-        } else {
-            component = new Component(actionId.at(KGlobalAccel::ComponentUnique), actionId.at(KGlobalAccel::ComponentFriendly), m_registry);
-        }
-        Q_ASSERT(component);
+    const QString uniqueId = actionId.at(KGlobalAccel::ComponentUnique);
+
+    // If a component for action already exists, use that...
+    if (Component *c = m_registry->getComponent(uniqueId)) {
+        return c;
     }
-    return component;
+
+    // ... otherwise, create a new one
+    const QString friendlyId = actionId.at(KGlobalAccel::ComponentFriendly);
+    if (uniqueId.endsWith(QLatin1String(".desktop"))) {
+        auto *actionComp = m_registry->createSAComponent(uniqueId, friendlyId);
+        Q_ASSERT(actionComp);
+        actionComp->activateGlobalShortcutContext(QStringLiteral("default"));
+        actionComp->loadFromService();
+        return actionComp;
+    } else {
+        auto *comp = m_registry->createComponent(uniqueId, friendlyId);
+        Q_ASSERT(comp);
+        return comp;
+    }
 }
 
 GlobalShortcut *KGlobalAccelDPrivate::addAction(const QStringList &actionId)

KDE/kglobalaccel

src/runtime/globalshortcutsregistry.cpp
@@ -110,17 +110,10 @@ GlobalShortcutsRegistry::~GlobalShortcutsRegistry()
     _keys_count.clear();
 }
 
-Component *GlobalShortcutsRegistry::addComponent(Component *component)
+Component *GlobalShortcutsRegistry::registerComponent(Component *component)
 {
-    auto it = findByName(component->uniqueName());
-    if (it != m_components.cend()) {
-        Q_ASSERT_X(false, "GlobalShortcutsRegistry::addComponent", "component already registered");
-        return *it;
-    }
-
     m_components.push_back(component);
     QDBusConnection conn(QDBusConnection::sessionBus());
-
     conn.registerObject(component->dbusPath().path(), component, QDBusConnection::ExportScriptableContents);
     return component;
 }
@@ -130,11 +123,6 @@ void GlobalShortcutsRegistry::activateShortcuts()
     for (Component *component : m_components) {
         component->activateShortcuts();
     }
-}
-
-const std::vector<Component *> &GlobalShortcutsRegistry::allMainComponents() const
-{
-    return m_components;
 }
 
 QList<QDBusObjectPath> GlobalShortcutsRegistry::componentsDbusPaths() const
@@ -328,6 +316,34 @@ bool GlobalShortcutsRegistry::keyReleased(int keyQt)
     return false;
 }
 
+Component *GlobalShortcutsRegistry::createComponent(const QString &uniqueName, const QString &friendlyName)
+{
+    auto it = findByName(uniqueName);
+    if (it != m_components.cend()) {
+        Q_ASSERT_X(false, //
+                   "GlobalShortcutsRegistry::createComponent",
+                   QLatin1String("A Component with the name: %1, already exists").arg(uniqueName).toUtf8().constData());
+        return *it;
+    }
+
+    auto *c = registerComponent(new Component(uniqueName, friendlyName));
+    return c;
+}
+
+KServiceActionComponent *GlobalShortcutsRegistry::createServiceActionComponent(const QString &uniqueName, const QString &friendlyName)
+{
+    auto it = findByName(uniqueName);
+    if (it != m_components.cend()) {
+        Q_ASSERT_X(false, //
+                   "GlobalShortcutsRegistry::createServiceActionComponent",
+                   QLatin1String("A Component with the name: %1, already exists").arg(uniqueName).toUtf8().constData());
+        return static_cast<KServiceActionComponent *>(*it);
+    }
+
+    auto *c = registerComponent(new KServiceActionComponent(uniqueName, friendlyName));
+    return static_cast<KServiceActionComponent *>(c);
+}
+
 void GlobalShortcutsRegistry::loadSettings()
 {
     const auto groupList = _config.groupList();
@@ -346,13 +362,10 @@ void GlobalShortcutsRegistry::loadSettings()
         // that
         const QString friendlyName = configGroup.readEntry("_k_friendly_name");
 
+        const bool isDesktop = groupName.endsWith(QLatin1String(".desktop"));
         // Create the component
-        Component *component = nullptr;
-        if (groupName.endsWith(QLatin1String(".desktop"))) {
-            component = new KServiceActionComponent(groupName, friendlyName, this);
-        } else {
-            component = new Component(groupName, friendlyName, this);
-        }
+        Component *component = isDesktop ? createServiceActionComponent(groupName, friendlyName) //
+                                         : createComponent(groupName, friendlyName);
 
         // Now load the contexts
         const auto groupList = configGroup.groupList();
@@ -387,14 +400,14 @@ void GlobalShortcutsRegistry::loadSettings()
             continue;
         }
 
-        KDesktopFile f(file);
-        if (f.noDisplay()) {
+        KDesktopFile deskF(file);
+        if (deskF.noDisplay()) {
             continue;
         }
 
-        KServiceActionComponent *component = new KServiceActionComponent(fileName, f.readName(), this);
-        component->activateGlobalShortcutContext(QStringLiteral("default"));
-        component->loadFromService();
+        auto *actionComp = createServiceActionComponent(fileName, deskF.readName());
+        actionComp->activateGlobalShortcutContext(QStringLiteral("default"));
+        actionComp->loadFromService();
     }
 }
 

src/runtime/globalshortcutsregistry.h
@@ -10,6 +10,7 @@
 #include "kglobalaccel.h"
 
 #include "component.h"
+#include "kserviceactioncomponent.h"
 
 #include <KSharedConfig>
 
@@ -56,11 +57,6 @@ public:
      * Activate all shortcuts having their application present.
      */
     void activateShortcuts();
-
-    /**
-     * Return a list of all main components
-     */
-    const std::vector<Component *> &allMainComponents() const;
 
     /**
      * Returns a list of D-Bus paths of registered Components.
@@ -140,11 +136,15 @@ public Q_SLOTS:
     void ungrabKeys();
 
 private:
+    friend class KGlobalAccelDPrivate;
     friend class Component;
     friend class KGlobalAccelInterface;
     friend class KGlobalAccelInterfaceV2;
 
-    Component *addComponent(Component *component);
+    Component *createComponent(const QString &uniqueName, const QString &friendlyName);
+    KServiceActionComponent *createServiceActionComponent(const QString &uniqueName, const QString &friendlyName);
+
+    Component *registerComponent(Component *component);
     Component *takeComponent(Component *component);
 
     // called by the implementation to inform us about key presses

src/runtime/kglobalacceld.cpp
@@ -104,19 +104,26 @@ GlobalShortcut *KGlobalAccelDPrivate::findAction(const QString &_componentUnique
 
 Component *KGlobalAccelDPrivate::component(const QStringList &actionId) const
 {
-    // Get the component for the action. If we have none create a new one
-    Component *component = m_registry->getComponent(actionId.at(KGlobalAccel::ComponentUnique));
-    if (!component) {
-        if (actionId.at(KGlobalAccel::ComponentUnique).endsWith(QLatin1String(".desktop"))) {
-            component = new KServiceActionComponent(actionId.at(KGlobalAccel::ComponentUnique), actionId.at(KGlobalAccel::ComponentFriendly), m_registry);
-            component->activateGlobalShortcutContext(QStringLiteral("default"));
-            static_cast<KServiceActionComponent *>(component)->loadFromService();
-        } else {
-            component = new Component(actionId.at(KGlobalAccel::ComponentUnique), actionId.at(KGlobalAccel::ComponentFriendly), m_registry);
-        }
-        Q_ASSERT(component);
+    const QString uniqueName = actionId.at(KGlobalAccel::ComponentUnique);
+
+    // If a component for action already exists, use that...
+    if (Component *c = m_registry->getComponent(uniqueName)) {
+        return c;
     }
-    return component;
+
+    // ... otherwise, create a new one
+    const QString friendlyName = actionId.at(KGlobalAccel::ComponentFriendly);
+    if (uniqueName.endsWith(QLatin1String(".desktop"))) {
+        auto *actionComp = m_registry->createServiceActionComponent(uniqueName, friendlyName);
+        Q_ASSERT(actionComp);
+        actionComp->activateGlobalShortcutContext(QStringLiteral("default"));
+        actionComp->loadFromService();
+        return actionComp;
+    } else {
+        auto *comp = m_registry->createComponent(uniqueName, friendlyName);
+        Q_ASSERT(comp);
+        return comp;
+    }
 }
 
 GlobalShortcut *KGlobalAccelDPrivate::addAction(const QStringList &actionId)